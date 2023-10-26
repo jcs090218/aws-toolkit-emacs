da637eec8d1b10aba70df338865ca6d9e4898347
JenChieh <jcs090218@gmail.com>
2023-10-26 00:41:31 -0700

    add src

--- aws.el
+++ aws.el
@@ -4,7 +4,7 @@
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
-;; URL: https://github.com/jcs090218/aws
+;; URL: https://github.com/jcs090218/aws-toolkit-emacs
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: aws convenience
