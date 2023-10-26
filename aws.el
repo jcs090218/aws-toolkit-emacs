e3e0dfb30237a94b85d15b213f4b468b705db243
JenChieh <jcs090218@gmail.com>
2023-10-26 00:29:33 -0700

    Add src

---
+++ aws.el
@@ -0,0 +1,37 @@
+;;; aws.el --- AWS Toolkit  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/jcs090218/aws
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: aws convenience
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; AWS Toolkit
+;;
+
+;;; Code:
+
+
+
+(provide 'aws)
+;;; aws.el ends here
