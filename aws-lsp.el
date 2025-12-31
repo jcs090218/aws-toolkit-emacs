59250e6a2d5e7cd0e4d9ce9878d191e62e5f47e6
JenChieh <jcs090218@gmail.com>
2026-01-01 05:05:32 +0800

    chore: Update copyright info

--- aws-lsp.el
+++ aws-lsp.el
@@ -1,6 +1,6 @@
 ;;; aws-lsp.el --- LSP related  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023-2024  Shen, Jen-Chieh
+;; Copyright (C) 2023-2026  Shen, Jen-Chieh
 
 ;; This file is not part of GNU Emacs.
 
